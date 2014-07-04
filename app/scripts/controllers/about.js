0dc7fc3b55d0f0d739d0775dc05ee66bc292d815
Louis Wilbrink Air <louis.wilbrink@gmail.com>
2014-07-04 16:07:31 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fasterScaleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fasterScaleApp
+ */
+angular.module('fasterScaleApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fasterScaleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fasterScaleApp
+ */
+angular.module('fasterScaleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
